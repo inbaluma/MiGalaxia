9a282e622736dc191bd9b6be6fd79c5c36cd718e
DavidMorillasCastillo <0619993041@uma.es>
2024-05-12 19:51:13 +0200

    Avance Trivia

--- migalaxia2/src/Components/Juegos/Trivia.js
+++ migalaxia2/src/Components/Juegos/Trivia.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Trivia.css'
 
 import "../../index.css"
 import Navigation from "../Navigation";
@@ -7,7 +8,20 @@ function trivia (){
     return(
         <div className="mx-3">
             <Navigation actual="Trivia" paginas={[{nombre: "Jugar", path:"/jugar"}]}/>
-            <h1 className="titulo mb-3">TRIVIA</h1>            
+            <div className="contenedor">
+                <h1 className="titulo mb-3">TRIVIA</h1>
+                <hr />
+                <h2>¿Quién está mas calvo?</h2>
+                <ul>
+                    <li>Guardiola</li>
+                    <li>Iniesta</li>
+                    <li>Zidane</li>
+                    <li>Nadal</li>
+                </ul>
+                <button>Siguiente</button>
+                <div className="indice">1 de 20 preguntas</div>
+            </div>
+                        
         </div>
     );
 }
