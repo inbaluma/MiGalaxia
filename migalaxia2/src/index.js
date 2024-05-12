a40c67db235ed974cdbee97964924a4262308288
DavidMorillasCastillo <0619993041@uma.es>
2024-05-12 19:55:30 +0200

    Idea fondo pero no me sale no se porque
    Los .space y .stars no se ven en la pagina no se porque, estaré equivocandome en alguna estupidez

--- migalaxia2/src/index.js
+++ migalaxia2/src/index.js
@@ -9,6 +9,17 @@ root.render(
       <App/>
     </React.StrictMode>
   );
+
+  function prueba() {
+    return (
+      <div className='space'>
+        <div className='stars1'></div>
+        <div className='stars2'></div>
+        <div className='stars3'></div>
+      </div>
+    );
+  }
+  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
