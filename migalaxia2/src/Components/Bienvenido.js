01fde6cf19b233a1cecbd67c36a085b843373e9d
DavidMorillasCastillo <0619993041@uma.es>
2024-05-14 19:59:16 +0200

    Subo esto que me ponia nervioso como estaba

--- migalaxia2/src/Components/Bienvenido.js
+++ migalaxia2/src/Components/Bienvenido.js
@@ -3,7 +3,7 @@ import imagenBienvenida from "./imagenBienvenida.jpg";
 function Bienvenido() {
     return (
         <div style={{ textAlign: 'center' }}>
-            <h1 className="titulo" style={{ fontSize: '3rem', marginBottom: '20px' }}>Bienvenido a tu galaxia</h1>
+            <h1 className="titulo" style={{ fontSize: '3rem', marginBottom: '20px' }}>Bienvenido a MiGalaxia</h1>
             <div style={{ margin: 'auto', width: '50%' }}>
                 <p style={{ fontSize: '1.5rem' }}>El lugar perfecto para aprender sobre nuestra galaxia</p>
             </div>
