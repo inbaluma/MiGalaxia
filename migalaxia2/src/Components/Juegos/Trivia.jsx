f87d9da010f2dcd8f3cbfec2f45006ed7f0a64fa
DavidMorillasCastillo <0619993041@uma.es>
2024-05-22 18:40:58 +0200

    Trivia terminado a falta de hacer las preguntas
    Mas adelante hacemos las preguntas basadas en la info que metamos en la sección aprender

--- migalaxia2/src/Components/Juegos/Trivia.jsx
+++ migalaxia2/src/Components/Juegos/Trivia.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Trivia.css'
 import "../../index.css"
 
@@ -9,13 +9,55 @@ const Trivia = () => {
 
     let [index, setIndex] = useState(0);
     let [question,setQuestion] = useState(data[index]);
+    let [lock,setLock] = useState(false);
+    let [score, setScore] = useState(0);
+    let[result, setResult] = useState(false);
+
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let option_array = [Option1, Option2, Option3, Option4];
 
     const checkAns = (e,ans) => {
-        if (question.ans===ans){
-            e.target.classList.add("correct");
-        }else{
-            e.target.classList.add("wrong")
+        if (lock === false){
+            if (question.ans===ans){
+                e.target.classList.add("correct");
+                setLock(true);
+                setScore(prev=>prev+1);
+            }else{
+                e.target.classList.add("wrong");
+                setLock(true);
+                option_array[question.ans-1].current.classList.add("correct");
+            }
         }
+        
+    }
+
+    const next = () => {
+        if(lock===true){
+            if(index === data.length-1){
+                setResult(true);
+                return 0;
+            }
+            setIndex(++index);
+            setQuestion(data[index]);
+            setLock(false);
+            option_array.map((option)=>{
+                option.current.classList.remove("wrong");
+                option.current.classList.remove("correct");
+                return null;
+            })
+        }
+    }
+
+    const reset = () => {
+        setIndex(0);
+        setQuestion(data[0]);
+        setScore(0);
+        setLock(false);
+        setResult(false);
     }
 
     return (
@@ -24,15 +66,20 @@ const Trivia = () => {
             <div className="contenedor">
                 <h1 className="titulo mb-3">TRIVIA</h1>
                 <hr />
-                <h2>{index +1}.{question.question}</h2>
+                {result?<></>:<><h2>{index +1}.{question.question}</h2>
                 <ul>
-                    <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-                    <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
-                    <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
-                    <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+                    <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+                    <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+                    <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+                    <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
                 </ul>
-                <button>Siguiente</button>
-                <div className="indice">1 de 20 preguntas</div>
+                <button onClick={next}>Siguiente</button>
+                <div className="indice">{index+1} de {data.length} preguntas</div></>}
+                {result?<>
+                    <h2>Tu puntuación es {score} de {data.length}</h2>
+                    <button onClick={reset}>Reiniciar</button>
+                </>:<></>}
+               
             </div>
                         
         </div>
